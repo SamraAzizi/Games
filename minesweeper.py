004502c657c4b8d25ea8c78eca2cc8f1d6ce5c83
SamraAzizi <samarraazizi@gmail.com>
2024-03-09 23:02:01 +0500

    my commit

--- minesweeper.py
+++ minesweeper.py
@@ -12,17 +12,6 @@ class Minesweeper:
         self.buttons = [[None] * cols for _ in range(rows)]
         self.game_over = False
 
-        self.init_board()
-        self.place_mines()
-        self.calculate_numbers()
-
-        self.create_widgets()
-
-    def init_board(self):
-        for i in range(self.rows):
-            for j in range(self.cols):
-                button = tk.Button(self.master, width=2, height=1, font=('Helvetica', 14), command=lambda i=i, j=j: self.click(i, j))
-                button.grid(row=i, column=j)
-                self.buttons[i][j] = button
+ 
 
  
