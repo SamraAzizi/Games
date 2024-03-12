49a15f0be3b7ac83add60cadb759f4e73bc5e680
SamraAzizi <samarraazizi@gmail.com>
2024-03-12 23:36:05 +0500

    my commit

--- match_pair.py
+++ match_pair.py
@@ -3,24 +3,4 @@ import random
 
 class MemoryGame:
     def __init__(self, master, size=4):
-        self.master = master
-        self.size = size
-        self.symbols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']
-        self.board = self.initialize_board()
-        self.revealed = set()
-        self.buttons = [[None] * size for _ in range(size)]
-        self.create_board()
-        self.first_click = None
-
-    def initialize_board(self):
-        num_pairs = self.size * self.size // 2
-        pairs = random.sample(self.symbols, num_pairs) * 2
-        random.shuffle(pairs)
-        random.shuffle(pairs)
-        board = [[None] * self.size for _ in range(self.size)]
-        for i in range(self.size):
-            for j in range(self.size):
-                board[i][j] = (pairs.pop(), random.choice(['red', 'green', 'blue', 'yellow', 'orange', 'purple']))
-        return board
-
- 
+    
