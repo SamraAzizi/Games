5f8de5543e32fa5398798e244e6fb4133ab27440
SamraAzizi <samarraazizi@gmail.com>
2024-03-14 22:56:28 +0500

    my commit

--- match_pair.py
+++ match_pair.py
@@ -31,4 +31,40 @@ class MemoryGame:
                 button.grid(row=i, column=j, padx=5, pady=5)
                 self.buttons[i][j] = button
 
- 
+    def reveal(self, row, col):
+        if (row, col) not in self.revealed:
+            symbol, color = self.board[row][col]
+            self.buttons[row][col]['text'] = symbol
+            self.buttons[row][col]['bg'] = color
+            self.revealed.add((row, col))
+            if self.first_click is None:
+                self.first_click = (row, col)
+            else:
+                self.master.after(1000, self.check_match)
+
+    def check_match(self):
+        row1, col1 = self.first_click
+        row2, col2 = next(iter(self.revealed - {(row1, col1)}))
+        if self.board[row1][col1][0] == self.board[row2][col2][0]:
+            self.buttons[row1][col1]['state'] = 'disabled'
+            self.buttons[row2][col2]['state'] = 'disabled'
+            self.revealed.clear()
+            if all(all(cell['state'] == 'disabled' for cell in row) for row in self.buttons):
+                tk.messagebox.showinfo("Game Over", "Congratulations! You won!")
+        else:
+            self.buttons[row1][col1]['text'] = ''
+            self.buttons[row1][col1]['bg'] = 'SystemButtonFace'
+            self.buttons[row2][col2]['text'] = ''
+            self.buttons[row2][col2]['bg'] = 'SystemButtonFace'
+            self.revealed.clear()
+        self.first_click = None
+
+def main():
+    root = tk.Tk()
+    root.title("Memory Game")
+    size = 4  # Change the size of the board as desired
+    game = MemoryGame(root, size=size)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
