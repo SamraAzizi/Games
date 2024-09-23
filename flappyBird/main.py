2904e3b58eabab416c9c58cd3ebe45b3a151cfa1
SamraAzizi <samarraazizi@gmail.com>
2024-09-23 19:34:36 +0500

    flappyBird

--- flappyBird/main.py
+++ flappyBird/main.py
@@ -8,3 +8,9 @@ SCREENWIDTH = 289
 SCREENHEIGHT = 511
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 GROUNDY = SCREENHEIGHT * 0.8
+GAME_SPRITES = {}
+GAME_SOUNDS = {}
+PLAYER = 'gallery/bird.png'
+BACKGROUND = 'gallery/background.png'
+PIPE = 'gallery/pipe.png'
+
