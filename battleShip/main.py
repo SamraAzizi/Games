273c8bdb0c7c1b974f7733b8883b15bda6cb6359
SamraAzizi <samarraazizi@gmail.com>
2024-10-03 22:24:28 +0500

    battleship

--- battleShip/main.py
+++ battleShip/main.py
@@ -3,7 +3,7 @@
 hiden_board = [[' '] * 8 for x in range[8]]
 guess_board = [[' '] * 8 for x in range[8]]
 
-letters_to_numbers = 
+letters_to_numbers = {'a' :0, 'b' : 1, 'c' : 2, 'd' :3, 'e' : 4, 'f':5, 'g' : 6, 'h' : 7}
 
 def board():
     pass
