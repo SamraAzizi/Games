8625aa586942f7d9a4afefb7eef7046ebc7f35a2
SamraAzizi <fahimaazizi369@gmail.com>
2024-10-04 22:51:56 +0500

    battleship

--- battleShip/main.py
+++ battleShip/main.py
@@ -6,7 +6,14 @@ guess_board = [[' '] * 8 for x in range[8]]
 letters_to_numbers = {'a' :0, 'b' : 1, 'c' : 2, 'd' :3, 'e' : 4, 'f':5, 'g' : 6, 'h' : 7}
 
 def print_board(board):
-    pass
+
+    print(' A B C D E F G H')
+    print(' ==================')
+
+    row_num = 1
+    for row in board:
+        print("%d|%s|" %(row_num))
+    
 
 def create_ships():
     pass
