ea44c7e23965776af947dddb0d8ed7ccbd2726c6
SamraAzizi <samarraazizi@gmail.com>
2024-10-04 22:45:24 +0500

    battleship

--- battleShip/main.py
+++ battleShip/main.py
@@ -5,7 +5,7 @@ guess_board = [[' '] * 8 for x in range[8]]
 
 letters_to_numbers = {'a' :0, 'b' : 1, 'c' : 2, 'd' :3, 'e' : 4, 'f':5, 'g' : 6, 'h' : 7}
 
-def board():
+def print_board(board):
     pass
 
 def create_ships():
@@ -17,3 +17,7 @@ def get_ship_location():
 
 def count_hit_ships():
     pass
+
+create_ships()
+turns = 10
+##while turns > 0 
