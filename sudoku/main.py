27d332ff19376cb23b5fe7ea2e0bc35afe5f87a9
SamraAzizi <samarraazizi@gmail.com>
2024-10-09 20:16:01 +0500

    sudoku

--- sudoku/main.py
+++ sudoku/main.py
@@ -1,5 +1,8 @@
 
-
+def fint_next_empty(puzzle):
+    for r in range(9):
+        for c in range(9):
+            if puzzle()
 def solve_sudoku(puzzle):
     #solve sudoku using backtracking!
     # our puzzle is a list of lists where each inner list is a row
@@ -8,3 +11,4 @@ def solve_sudoku(puzzle):
     # mutates puzzle to be the solution
 
     #1 choose somewhere on the puzzle to make a guess
+    row, col = find_next_empty(puzzle)
