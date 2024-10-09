87e6c307aa7327d21da24facf1d63db2a24e647a
SamraAzizi <fahimaazizi369@gmail.com>
2024-10-09 20:11:34 +0500

    sudoku_game

---
+++ sudoku/main.py
@@ -0,0 +1,8 @@
+
+
+def solve_sudoku(puzzle):
+    #solve sudoku using backtracking!
+    # our puzzle is a list of lists where each inner list is a row
+    # in our sudoku puzzle
+    #return whether a solution exits
+    # mutates puzzle to be the solution
