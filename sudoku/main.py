effa7276ca7f332df92717823e204a681667dd77
SamraAzizi <samarraazizi@gmail.com>
2024-10-09 20:35:45 +0500

    sudoku

--- sudoku/main.py
+++ sudoku/main.py
@@ -7,11 +7,13 @@ def fint_next_empty(puzzle):
             
     return None, None
 def solve_sudoku(puzzle):
-    #solve sudoku using backtracking!
-    # our puzzle is a list of lists where each inner list is a row
-    # in our sudoku puzzle
-    #return whether a solution exits
-    # mutates puzzle to be the solution
-
+    
     #1 choose somewhere on the puzzle to make a guess
     row, col = find_next_empty(puzzle)
+
+    if row is None:
+        return True
+    
+    for guess in range(1,10):
+        
+
